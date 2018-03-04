639cab7a04ef3720ebd9572759b8766b03b8bac8
Thuan Vo <35320089+tvo93@users.noreply.github.com>
2018-03-03 20:23:58 -0800

    Update Address.cs

--- ContosoUniversity/Models/Address.cs
+++ ContosoUniversity/Models/Address.cs
@@ -8,8 +8,7 @@ namespace ContosoUniversity.Models
 {
     public abstract class Address
     {
-       public int ID { get; set; }
-
+     
         [EmailAddress]
         [RegularExpression(@"^[a-zA-Z0-9._]+@contoso\.edu$", ErrorMessage = "Invalid Email addresses. Please entere into the system conform with the pattern [username]@contoso.edu")]
         public string Email { get; set; }
